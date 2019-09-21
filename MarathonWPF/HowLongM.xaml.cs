ba725f95a38c301ff31e8844d635dffe87c5a59a
Павел Степанченко <stepanpahska@live.ru>
2019-09-21 18:09:26 +0300

    creating forms

---
+++ MarathonWPF/HowLongM.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MarathonWPF
+{
+    /// <summary>
+    /// Interaction logic for HowLongM.xaml
+    /// </summary>
+    public partial class HowLongM : Window
+    {
+        public HowLongM()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ MarathonWPF/ListFonds.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MarathonWPF
+{
+    /// <summary>
+    /// Interaction logic for ListFonds.xaml
+    /// </summary>
+    public partial class ListFonds : Window
+    {
+        public ListFonds()
+        {
+            InitializeComponent();
+        }
+    }
+}
